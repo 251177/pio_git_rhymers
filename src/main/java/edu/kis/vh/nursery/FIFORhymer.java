293688942179683f0c588050bc9560cd8f2f5847
Kacper Kwintera <251177@edu.p.lodz.pl>
2026-04-14 23:36:40 +0200

    17. Wygenerowanie i uzupelnienie dokumentacji Javadoc w klasie FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -4,10 +4,12 @@ package edu.kis.vh.nursery;
  * Kolejka FIFO (First In First Out) implementująca specyficzny wariant wyliczanki.
  */
 public class FIFORhymer extends DefaultCountingOutRhymer {
+
     /**
      * Zapasowy wyliczacz (stos) wykorzystywany do odwracania kolejności elementów.
      */
     private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+
     /**
      * Zwraca i usuwa pierwszy dodany element do wyliczanki (zgodnie z zasadą FIFO).
      *
